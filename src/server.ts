b482ab1b87c1d416be75565972b59139eca007e7
AlisaUrsu <115451781+AlisaUrsu@users.noreply.github.com>
2024-05-30 18:49:48 +0300

    changed

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,11 @@ import app from "./app";
 import "dotenv/config";
 
 const port = process.env.PORT || 5000;
-
+app.listen(port, () => {
+    console.log("Server running on port: " + port);
+})
 mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
     
         console.log("Mongoose connected.");
 
-    app.listen(port, () => {
-        console.log("Server running on port: " + port);
-    })
+    
